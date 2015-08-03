1f9f936f1ad5e5be70f6786cecbcfdbf05dd92cf
chriselly <chris.elly@me.com>
2015-08-03 12:59:45 -0400

    Render always original icon on TabBariOS

--- React/Views/RCTTabBarItem.m
+++ React/Views/RCTTabBarItem.m
@@ -51,10 +51,12 @@
   _icon = [icon copy];
 
   // Check if string matches any custom images first
-  UIImage *image = [RCTConvert UIImage:_icon];
+  //  UIImage *image = [RCTConvert UIImage:_icon];
+  UIImage *image = [[RCTConvert UIImage:_icon] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
   UITabBarItem *oldItem = _barItem;
   if (image) {
-    
+
     // Recreate barItem if previous item was a system icon
     if (wasSystemIcon) {
       _barItem = nil;
